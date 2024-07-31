8dca42841b3ff88ef0f80658df06534af3e22c1a
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-07-31 23:05:51 +0530

    comment

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,7 @@
-
+import React from "react";
+const Selection = () =>{
+    return <div id="main">
+    <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+}
+export default Selection;
